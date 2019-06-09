beeb4966a5f6c448fdda5dc3c28b8f492599bfce
Annabelle Thomas Taylor <annabelle.t.taylor@gmail.com>
2019-06-09 14:48:18 -0400

    Update state with selected race

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
 import React from 'react';
 import './App.css';
 import Results from './data/Results'
+import ResultData from './data/results_2004.json'
 
-function App() {
-  return (
-    <div className="App">
-      
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      selectedRace: ''
+    }
+    this.updateRace=this.updateRace.bind(this)
+  }
+
+  updateRace(event) {
+    this.setState({ selectedRace: event.target.value })
+  }
+
+  render() {
+    let demVote = 0
+    let repVote = 0
+    let otherVote = 0
+
+    let raceSelection = []
+
+    Object.entries(ResultData).forEach(([key,value]) => {
+      otherVote = 0
+      console.log(key)
+      raceSelection.push(<option value={key}>{key}</option>)
+      Object.entries(value).forEach(([key,value]) => {
+        if(value.parties[0]==="Republican"){
+          repVote = value.votes
+        }
+        else if (value.parties[0]==="Democratic"){
+          demVote = value.votes
+        }
+        else{
+          otherVote+=value.votes
+        }
+      })
+        // console.log(key.slice(-2))
+        // console.log("Republican votes: " + repVote)
+        // console.log("Democratic votes: " + demVote)
+        // console.log("Other votes: " + otherVote)
+    })
+    return (
+      <div>
+        <select value={this.state.selectedRace} onChange={this.updateRace}>
+          <option value="" disabled>Select a race (year/state)</option>
+          {raceSelection}
+        </select>
       <Results />
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/data/Results.js
+++ src/data/Results.js
@@ -4,32 +4,7 @@ import { Bar } from 'react-chartjs-2'
 
 
 class Results extends React.Component {
- 
-
   render(){
-    let demVote = 0
-    let repVote = 0
-    let otherVote = 0
-
-    Object.entries(ResultData).forEach(([key,value]) => {
-      otherVote = 0
-      Object.entries(value).forEach(([key,value]) => {
-        if(value.parties[0]==="Republican"){
-          repVote = value.votes
-        }
-        else if (value.parties[0]==="Democratic"){
-          demVote = value.votes
-        }
-        else{
-          otherVote+=value.votes
-        }
-      })
-        console.log(key.slice(-2))
-        console.log("Republican votes: " + repVote)
-        console.log("Democratic votes: " + demVote)
-        console.log("Other votes: " + otherVote)
-    })
-
     const data = {
       labels: ['Republican','Democrat','Other'],
       datasets: [
@@ -40,7 +15,7 @@ class Results extends React.Component {
           borderWidth: 1,
           hoverBackgroundColor: 'rgba(255,99,132,0.4)',
           hoverBorderColor: 'rgba(255,99,132,1)',
-          data: [repVote, demVote, otherVote]
+          data: [1, 2, 3]
         }
       ]
     };
@@ -48,7 +23,6 @@ class Results extends React.Component {
     
     return (
       <div>
-        hello world!
         <Bar data={data} />
       </div>
     )
