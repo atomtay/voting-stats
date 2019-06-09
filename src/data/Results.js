d6a9851f2094534cce62bfcdcc5bf0d613939415
Annabelle Thomas Taylor <annabelle.t.taylor@gmail.com>
2019-06-09 14:29:35 -0400

    Render single bar chart

--- src/data/Results.js
+++ src/data/Results.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ResultData from './results_2004.json'
+import { Bar } from 'react-chartjs-2'
 
 
 class Results extends React.Component {
+ 
+
   render(){
     let demVote = 0
     let repVote = 0
     let otherVote = 0
 
-
     Object.entries(ResultData).forEach(([key,value]) => {
+      otherVote = 0
       Object.entries(value).forEach(([key,value]) => {
         if(value.parties[0]==="Republican"){
           repVote = value.votes
@@ -27,12 +30,26 @@ class Results extends React.Component {
         console.log("Other votes: " + otherVote)
     })
 
+    const data = {
+      labels: ['Republican','Democrat','Other'],
+      datasets: [
+        {
+          label: 'Votes',
+          backgroundColor: 'rgba(255,99,132,0.2)',
+          borderColor: 'rgba(255,99,132,1)',
+          borderWidth: 1,
+          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+          hoverBorderColor: 'rgba(255,99,132,1)',
+          data: [repVote, demVote, otherVote]
+        }
+      ]
+    };
     
     
     return (
       <div>
         hello world!
-        
+        <Bar data={data} />
       </div>
     )
   }
