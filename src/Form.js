188112cdcb873cdb61695447073dcb3b35f72579
Annabelle Thomas Taylor <annabelle.t.taylor@gmail.com>
2019-06-09 19:13:05 -0400

    Collect info from form

--- src/Form.js
+++ src/Form.js
@@ -4,14 +4,36 @@ class Form extends React.Component {
     constructor(props){
         super(props)
         this.state={
+            candidate: '',
+            state: '',
+            votes: 0,
             nodes: []
         }
         this.addNode = this.addNode.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
 
     addNode(event){
         event.preventDefault()
-        console.log("Submit!")
+        console.log(this.state.candidate)
+        const formName = this.state.candidate
+        const formState = this.state.state
+        const formVotes = this.state.votes
+        const newCandidate = new Object()
+        newCandidate.name = formName
+        
+        
+
+
+        console.log(newCandidate)
+        let oldNodes = this.state.nodes
+        let newNodes = oldNodes.push(newCandidate)
+        console.log(newNodes)
+        this.setState({nodes: newNodes})
+    }
+
+    handleChange(event){
+        this.setState({[event.target.name]: event.target.value})
     }
 
 
@@ -23,13 +45,14 @@ class Form extends React.Component {
 
         return (
         <div>
-            <form>
-                <input type="text" name="candidate" />
-                <br/><select>
-                    {formStateSelect}
-                </select>
-                <br/><input type="number" name="votes" />
-                <br/><input type="submit" onClick={this.addNode}/>
+            <form onSubmit={this.addNode}>
+                <input type="text" name="candidate" value={this.state.value} onChange={this.handleChange}/>
+                <br/><select value={this.state.state} name="state" onChange={this.handleChange}>
+          <option value="" disabled>Select a state</option>
+          {formStateSelect}
+        </select>
+                <br/><input type="number" name="votes" value={this.state.value} onChange={this.handleChange}/>
+                <br/><input type="submit"/>
             </form>
         </div>
         )
